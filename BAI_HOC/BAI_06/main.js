7f96e1ab08a026d2255193ebc1eef71c90c0e08c
HoaiNhu <22521051@gm.uit.edu.vn>
2024-08-20 19:04:27 +0700

    thutuuutien

---
+++ BAI_HOC/BAI_06/main.js
@@ -0,0 +1,16 @@
+//các phép toán cơ bản
+let a = 5;
+let b = 2;
+
+let kq1 = a + b;
+let kq2 = a - b;
+let kq3 = a * b;
+let kq4 = a / b;
+let kq5 = a % b;
+
+// xuất kq
+console.log("Tổng 2 số a và b = " + kq1);
+console.log("Hiệu 2 số a và b = " + kq2);
+console.log("Tích 2 số a và b = " + kq3);
+console.log("Thương 2 số a và b = " + kq4);
+console.log("Modulo 2 số a và b = " + kq5);
