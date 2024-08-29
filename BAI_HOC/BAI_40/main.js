417e9648deddd0a58311a903eecdb72d75a2b6d2
HoaiNhu <22521051@gm.uit.edu.vn>
2024-08-29 10:15:51 +0700

    đệ quy

--- BAI_HOC/BAI_40/main.js
+++ BAI_HOC/BAI_40/main.js
@@ -1,83 +1,24 @@
-//var let const
+// tính n!
 
-console.log("Giá trị biến c = " + c); //undefined
-
-//khởi tạo biến c
-var c = 9;
-
-// JS sẽ hiểu như sau:
-//var c
-//console...
-//c= 9
-
-// console.log("Giá trị biến c = " + d);
-// let d = 9; //Cannot access 'd' before initialization
-
-// 2. phạm vi
-// var có phạm vi function scope
-function exampleVar() {
-  if (true) {
-    var y = 10; //y trong block scope
-    console.log(y + " trong block scope");
+function giaiThua(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * giaiThua(n - 1);
   }
-  console.log(y + " trong function scope");
 }
 
-exampleVar();
+let kq = giaiThua(5);
+console.log(kq);
 
-// js hiểu
-function exampleVar() {
-  var y; //đẩy y lên function scope
-  if (true) {
-    y = 10;
-    console.log(y + " trong block scope");
+// tính dãy fibonacci
+
+function f(n) {
+  if (n <= 2) return 1;
+  else {
+    return f(n - 1) + f(n - 2);
   }
-  console.log(y + " trong function scope");
 }
 
-exampleVar();
-
-// 3. let const có phạm vi block scope
-function exampleLetConst() {
-  if (true) {
-    let x = 10;
-    console.log(x + " trong block scope");
-  }
-  //   console.log(x + " trong function scope");// x is not defined at exampleLetConst
-}
-
-exampleLetConst();
-
-var diemToan = 9;
-//code
-var diemToan = 8;
-
-let diem = 9;
-// let diem = 8;
-
-// 4. hoisting in function
-
-// function declaration sẽ bị hoisting
-hoistedFunction();
-
-function hoistedFunction() {
-  console.log("hi");
-}
-
-// engine của trình duyệt đọc
-function hoistedFunction() {
-  console.log("hi");
-}
-
-hoistedFunction();
-
-// function expression và arrow function ko bị hoisting
-// unhoistedFunction(); // Cannot access 'unhoistedFunction' before initialization
-
-let unhoistedFunction = function () {
-  console.log("hi");
-};
-
-let nonhoistedFunction = () => {
-  console.log("hi");
-};
+let kq2 = f(10);
+console.log(kq2);
