ed4faced480f651efbba4ed18d2339d067f66e14
HoaiNhu <22521051@gm.uit.edu.vn>
2024-08-29 18:37:40 +0700

    trim

---
+++ BAI_HOC/BAI_43/main.js
@@ -0,0 +1,16 @@
+// trim
+let s1 = "        he      llo  world        ";
+console.log(s1.length);
+let s2 = s1.trim();
+console.log(s2);
+console.log(s2.length);
+
+// trimEnd
+let s3 = s1.trimEnd();
+console.log(s3);
+console.log(s3.length);
+
+// trimStart
+let s4 = s1.trimStart();
+console.log(s4);
+console.log(s4.length);
